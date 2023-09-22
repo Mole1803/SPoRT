07ac8b1d4ff2ca60d88b6b99ee5f1b72e4ce9be4
RuHi <ruben.hinz@continental.com>
2023-09-22 09:32:05 +0200

    Init backend

---
+++ SeaPortOptimizerBackend/src/Quest.py
@@ -0,0 +1,8 @@
+
+class Quest:
+    def __init__(self, name, id, is_active, resource, demand):
+        self.name = name
+        self.id = id
+        self.is_active = is_active
+        self.resource = resource
+        self.demand = demand
