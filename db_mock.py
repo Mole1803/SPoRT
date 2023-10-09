6b2cc7952b0a9f39292db712dfea560c1c434b55
Ruben Hinz <ruben.hinz@continental-corporation.com>
2023-10-09 17:55:42 +0200

    add mock

---
+++ db_mock.py
@@ -0,0 +1,42 @@
+class MockDBController:
+
+    @staticmethod
+    def get_ship(id):
+        pass
+
+    @staticmethod
+    def get_all_ships():
+        pass
+
+    @staticmethod
+    def create_ship(user, name, id, is_active, capacity):
+        pass
+
+    @staticmethod
+    def update_ship(user, name, id, is_active, capacity):
+        pass
+
+    @staticmethod
+    def delete_ship(id):
+        pass
+
+    @staticmethod
+    def get_all_quests():
+        pass
+
+    @staticmethod
+    def get_quest():
+        pass
+
+    @staticmethod
+    def create_quest(user, name, id, is_active, resource, items_per_capacity, demand):
+        pass
+
+    @staticmethod
+    def update_quest(user, name, id, is_active, resource, items_per_capacity, demand):
+        pass
+
+    @staticmethod
+    def delete_quest(id):
+        pass
+
