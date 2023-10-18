498fb40aaaac60b7cb68335a1c80dc3e32394ceb
Ruben Hinz <ruben.hinz@continental-corporation.com>
2023-10-18 15:18:35 +0200

    start algorithm and add basic test

--- SeaPortOptimizerBackend/src/Solver/RubenSolver.py
+++ SeaPortOptimizerBackend/src/Solver/RubenSolver.py
@@ -1,12 +1,26 @@
+from math import inf
+
 from SeaPortOptimizerBackend.src.Solver.Solver import Solver
 
 
+def calculate_optimal_step_solution(quest,ships):
+    spare=inf
+    for
+
+
+
 class RubenSolver(Solver):
-    def __init__(self,id):
-        super().__init__(id)
+    #def __init__(self,id):
+    #    super().__init__(id)
+
+    def __init__(self, ships, quests):
+        super().__init__(ships, quests)
 
     def calculate_time_optimized(self):
         pass
 
     def calculate_resource_optimized(self):
-        pass
+        optimal_substeps={}
+        for quest in self.quests:
+            optimal_substeps[quest.id] = calculate_optimal_step_solution(quest,self.ships)
+
--- SeaPortOptimizerBackend/src/Solver/Solver.py
+++ SeaPortOptimizerBackend/src/Solver/Solver.py
@@ -1,13 +1,16 @@
-from db_mock import MockDBController
 from abc import ABC, abstractmethod
-
-DBController = MockDBController()
+from instance import DBController
 
 
 class Solver(ABC):
-    def __init__(self, id):
-        self.ships = DBController.get_all_ships_from_user_id_db(id)
-        self.quests = DBController.get_all_quests_from_user_id_db(id)
+    #def __init__(self, id):
+    #    self.ships = DBController.get_all_ships_from_user_id_db(id)
+    #    self.quests = DBController.get_all_quests_from_user_id_db(id)
+
+    def __init__(self, ships, quests):
+        self.ships = ships
+        self.quests = quests
+
 
     @abstractmethod
     def calculate_time_optimized(self):
