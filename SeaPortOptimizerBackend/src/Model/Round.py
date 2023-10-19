4156062471b1123507fc42e8331491c9aff94dd4
Ruben Hinz <ruben.hinz@continental-corporation.com>
2023-10-19 12:02:37 +0200

    implement resource optimized algo

--- SeaPortOptimizerBackend/src/Model/Round.py
+++ SeaPortOptimizerBackend/src/Model/Round.py
@@ -2,3 +2,6 @@
 class Round:
     def __init__(self, steps):
         self.steps = steps
+
+    def add_step(self, step):
+        self.steps.append(step)
--- SeaPortOptimizerBackend/src/Solver/RubenSolver.py
+++ SeaPortOptimizerBackend/src/Solver/RubenSolver.py
@@ -1,16 +1,13 @@
 from math import inf
 
+from SeaPortOptimizerBackend.src.Model.Result import Result
+from SeaPortOptimizerBackend.src.Model.Round import Round
 from SeaPortOptimizerBackend.src.Solver.Solver import Solver
-
-
-def calculate_optimal_step_solution(quest,ships):
-    spare=inf
-    for
-
+from SeaPortOptimizerBackend.src.Model.Step import Step
 
 
 class RubenSolver(Solver):
-    #def __init__(self,id):
+    # def __init__(self,id):
     #    super().__init__(id)
 
     def __init__(self, ships, quests):
@@ -20,7 +17,98 @@ class RubenSolver(Solver):
         pass
 
     def calculate_resource_optimized(self):
-        optimal_substeps={}
+        optimal_substeps = {}
         for quest in self.quests:
-            optimal_substeps[quest.id] = calculate_optimal_step_solution(quest,self.ships)
+            quest.all_steps = []
+            self.calculate_all_steps(quest, self.ships, [])
+            optimal_substeps[quest.id] = quest.all_steps
+            self.filter_steps(quest)
+        solutions = self.arrange_steps(self.quests)
+        ships_by_solutions = self.filter_solutions_by_ships(solutions,self.ships)
+        best_solutions, rounds = self.get_best_solutions(ships_by_solutions)
+        results = self.parse_solutions_to_results(best_solutions, rounds)
+        print(results)
 
+    def parse_solutions_to_results(self,best_solutions,number_of_rounds):
+        results=[]
+        for solution in best_solutions:
+            rounds=[]
+            for round_index in range(number_of_rounds):
+                round=Round([])
+                for ship in solution:
+                    if len(solution[ship])>round_index:
+                        round.add_step(solution[ship][round_index])
+                rounds.append(round)
+            results.append(Result(rounds))
+        return results
+
+
+    def get_best_solutions(self, ships_by_solutions):
+        min = inf
+        best_solutions=[]
+        for solution in ships_by_solutions:
+            take_rounds = 0
+            for ship in solution:
+                if len(solution[ship]) > take_rounds:
+                    take_rounds = len(solution[ship])
+            if take_rounds < min:
+                min=take_rounds
+                best_solutions = [solution]
+            elif take_rounds == min:
+                best_solutions.append(solution)
+        return best_solutions,min
+
+    def filter_solutions_by_ships(self, solutions, ships):
+        ships_by_solutions=[]
+        for solution in solutions:
+            ship_quests = {}
+            for ship in ships:
+                ship_quests[ship.id] = []
+            for step in solution:
+                ship_quests[step.ship_id].append(step)
+            ships_by_solutions.append(ship_quests)
+        return ships_by_solutions
+
+    def arrange_steps(self, quests):
+        solutions=[]
+        for quest in quests:
+            if len(solutions)==0:
+                solutions=quest.all_steps
+            else:
+                new_solutions = []
+                for solution in solutions:
+                    for steps in quest.all_steps:
+                        new_solutions.append(solution+steps)
+                solutions=new_solutions
+        return solutions
+
+    # TODO in case there is no perfect solution find the best solution
+    def filter_steps(self, quest):
+        to_delete = []
+        for steps in quest.all_steps:
+            sum = 0
+            for step in steps:
+                sum += step.spare_capacity
+            if sum != quest.demand:
+                to_delete.append(steps)
+        for delete in to_delete:
+            quest.all_steps.remove(delete)
+
+    def calculate_all_steps(self, quest, ships, steps, index=0):
+        i = index
+        while i < len(ships):
+            if quest.remaining_demand > 0:
+                ship = ships[i]
+                quest.remaining_demand -= quest.items_per_capacity * ship.capacity
+                step = Step(ship.id, quest.id, quest.items_per_capacity * ship.capacity)
+                steps.append(step)
+                i = self.calculate_all_steps(quest, ships, steps, i)
+            else:
+                quest.all_steps.append(steps.copy())
+                last_step = steps.pop(-1)
+                quest.remaining_demand += last_step.spare_capacity
+                return i + 1
+        if len(steps) > 0:
+            last_step = steps.pop(-1)
+            quest.remaining_demand += last_step.spare_capacity
+        return index + 1
