8a690ce85684b2f2660c23a0657f639ae041ed79
moritzhartmann <mole-0@web.de>
2023-11-16 22:03:43 +0100

    Added Delete/Edit Confirmation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import multiprocessing
 import subprocess
 import os
-
+import webbrowser
+import time
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PYTHON_DIR = os.path.join(BASE_DIR, "venv", "bin", "python")
@@ -11,16 +12,12 @@ def start_backend():
     subprocess.run([PYTHON_DIR, "-m", "flask", "run"])
 
 def run_frontend():
-    # run angular
     subprocess.run(["ng", "serve", "--open"], cwd=FRONTEND_DIR)
 
 if __name__ == '__main__':
-    print(BASE_DIR)
-    #os.system( f"{BASE_DIR} -m flask run")
+    #start backend
     multiprocessing.Process(target=start_backend).start()
+    #start frontend
     multiprocessing.Process(target=run_frontend).start()
-
-
-
-    print("Running frontend")
-    #subprocess.run("")
+    time.sleep(5)
+    webbrowser.open("http://localhost:4200")
